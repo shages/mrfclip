616dd6cd29266983110df5933242f33c08bb809c
Erik Ronshagen <eronshagen@gmail.com>
2017-01-21 21:32:51 -0800

    Update tests

--- tests/t20-icc.tcl
+++ tests/t20-icc.tcl
@@ -13,13 +13,13 @@ set rdir [file join $dir results [lindex [file split [file rootname [info script
 set poly1 {
     {
         90.0180 58.2080 90.0180 58.4800 91.3140 58.4800 91.3140 58.2080
-    } 
+    }
     {
         90.2880 58.7520 90.2880 59.0240 91.5840 59.0240 91.5840 58.7520
-    } 
+    }
     {
         90.0720 58.4800 90.0720 58.7520 90.3420 58.7520 90.3420 58.4800
-    } 
+    }
     {
         91.3140 58.2080 91.3140 58.4800 92.6100 58.4800 92.6100 58.2080
     }
@@ -49,10 +49,12 @@ set poly2 [transform_poly $poly2]
 #set poly1 {{25 25 25 60 60 60 60 25} {60 25 60 60 120 60 120 25} {40 60 40 100 100 100 100 60} {60 100 60 160 200 160 200 100}}
 #set poly2 {{50 50 50 150 150 150 150 50}}
 
+set wh {800 800}
+set poly1 [_scale_poly $poly1 4 4]
+set poly2 [_scale_poly $poly2 4 4]
 
-_clip_test 0 0 {}       [list $poly1 $poly2] $rdir
-_clip_test 0 1 {OR}     [list $poly1 $poly2] $rdir
-_clip_test 0 2 {AND}    [list $poly1 $poly2] $rdir
-_clip_test 0 3 {XOR}    [list $poly1 $poly2] $rdir
-_clip_test 0 4 {NOT}    [list $poly1 $poly2] $rdir
-
+_clip_test 0 0 {}       [list $poly1 $poly2] $rdir $wh
+_clip_test 0 1 {OR}     [list $poly1 $poly2] $rdir $wh
+_clip_test 0 2 {AND}    [list $poly1 $poly2] $rdir $wh
+_clip_test 0 3 {XOR}    [list $poly1 $poly2] $rdir $wh
+_clip_test 0 4 {NOT}    [list $poly1 $poly2] $rdir $wh
--- tests/t21-icc-2.tcl
+++ tests/t21-icc-2.tcl
@@ -10,23 +10,63 @@ source [file join $dir scripts testutils.tcl]
 set rdir [file join $dir results [lindex [file split [file rootname [info script]]] end]]
 
 
-set poly2 {{50 50 50 150 150 150 150 50}}
+set poly2 {{75 50 75 150 150 150 150 50}}
+# 1 - same size left
 set poly1 {
-    {60 60 60 100 100 100 100 60}
-    {100 100 100 50 125 50 125 100}
-    {100 140 100 160 125 160 125 140}
+   {75 75 75 100 100 100 100 75 }
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
 }
-# case 2
+# 2 - same size right
 set poly1 {
-    {60 60 60 100 100 100 100 60}
-    {100 100 100 75 125 75 125 100}
-    {100 140 100 160 125 160 125 140}
+   {125 100 125 75 150 75 150 100}
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
+}
+# 3 - same size bottom
+set poly1 {
+   {100 100 100 125 125 125 125 100}
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
+}
+# 4 - same size top
+set poly1 {
+   {100 75 125 75 125 50 100 50}
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
 }
 
+# 5 - left side, taller
+set poly1 {
+   {75 50 75 100 100 100 100 50 }
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
+}
+# 6 - left side, even taller
+set poly1 {
+   {75 50 75 125 100 125 100 50}
+   {100 100 100 75 125 75 125 100}
+   {100 140 100 160 125 160 125 140}
+}
+# 7 - 6 plus bottom wide poly
+#set poly1 {
+#   {100 50 100 75 150 75 150 50}
+#   {75 50 75 125 100 125 100 50}
+#   {100 100 100 75 125 75 125 100}
+#   {100 140 100 160 125 160 125 140}
+#}
 
-_clip_test 0 0 {}       [list $poly1 $poly2] $rdir
-_clip_test 0 1 {OR}     [list $poly1 $poly2] $rdir
-_clip_test 0 2 {AND}    [list $poly1 $poly2] $rdir
-_clip_test 0 3 {XOR}    [list $poly1 $poly2] $rdir
-_clip_test 0 4 {NOT}    [list $poly1 $poly2] $rdir
+#set poly1 {
+#  {75 50 75 150 125 150 125 100 150 100 150 50}
+#}
 
+set poly1 [_scale_poly $poly1 4 4]
+set poly2 [_scale_poly $poly2 4 4]
+
+set wh {800 800}
+
+_clip_test 0 0 {}       [list $poly1 $poly2] $rdir $wh
+_clip_test 0 1 {OR}     [list $poly1 $poly2] $rdir $wh
+_clip_test 0 2 {AND}    [list $poly1 $poly2] $rdir $wh
+_clip_test 0 3 {XOR}    [list $poly1 $poly2] $rdir $wh
+_clip_test 0 4 {NOT}    [list $poly1 $poly2] $rdir $wh
